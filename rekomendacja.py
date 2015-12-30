8522108322816c25451710c9e2cb766340eed976
dawidkol <dawid.kolano@gmail.com>
2015-12-30 19:00:58 +0100

    Lab3

--- rekomendacja.py
+++ rekomendacja.py
@@ -29,7 +29,7 @@ def manhattan(rating1, rating2):
     
     """Oblicz odległość w metryce taksówkowej między dwoma  zbiorami ocen
        danymi w postaci: {'The Strokes': 3.0, 'Slightly Stoopid': 2.5}
-       Zwróć -1, gdy zbiory nie mają… wspólnych elementów"""
+       Zwróć -1, gdy zbiory nie mają wspólnych elementów"""
        
     # TODO: wpisz kod
     klucze1 = rating1.keys()
@@ -49,10 +49,34 @@ def manhattan(rating1, rating2):
 
 def pearson(rating1, rating2):
     korelacja=0
-    
+
+    Exy = 0
+    Ex = 0
+    Ey = 0
+    Ex2 = 0
+    Ey2 = 0
+    n = 0
+    for klucz in rating1.keys():
+        if klucz in rating2.keys():
+            n = n + 1
+            x = rating1[klucz]
+            y = rating2[klucz]
+            Exy += x * y
+            Ex += x
+            Ey += y
+            Ex2 += pow(x, 2)
+            Ey2 += pow(y, 2)
+    korelacja = (Exy - (Ex * Ey) / n) / (sqrt(Ex2 - pow(Ex, 2) / n) * sqrt(Ey2 - pow(Ey, 2) / n))
     return korelacja
 
 def pearsonNumpy(rating1, rating2):
-    
+
     korelacja=0
+    x=[]
+    y=[]
+    for klucz in rating1.keys():
+        if klucz in rating2.keys():
+            x.append(rating1[klucz])
+            y.append(rating2[klucz])
+    korelacja = numpy.corrcoef(x,y)[0,1]
     return korelacja
